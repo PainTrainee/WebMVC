99ffc9f0b271e40267fa6f16b319a18ac377aefc
PainTrainee <vtec8197@gmail.com>
2023-11-25 11:40:49 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>()
+            {
+                new Product() { Id=1,Name="Coffee",Price=100,Amount=25},
+                new Product() { Id=2,Name="Coffee",Price=200,Amount=15},
+
+            };
+            return View(product);
+        }
+    }
+}
