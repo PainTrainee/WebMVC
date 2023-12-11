9054d231f1c8f451e2dc8d9f71c1a69fdc4ff937
PainTrainee <vtec8197@gmail.com>
2023-12-11 09:31:12 +0700

    Database: Code First

---
+++ P03_CodeFirst/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_CodeFirst.Models;
+
+namespace P03_CodeFirst.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=LAPTOP-M8KR7TQE;Database=TestProductXXX;Trusted_Connection=True;TrustServerCertificate=True;");
+            //optionsBuilder.UseSqlite("Data Source=TestProduct67");
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
