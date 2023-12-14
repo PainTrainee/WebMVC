c0a18418369bf18993d719f5f653f5cb5a0ac52e
PainTrainee <vtec8197@gmail.com>
2023-12-14 15:11:13 +0700

    One To Many

---
+++ P04_RelationDB/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P04_RelationDB.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=LAPTOP-M8KR7TQE;Database=TestProduct66;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
