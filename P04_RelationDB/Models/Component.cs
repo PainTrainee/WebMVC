e2756028b2bb1a15133fa0881b6868212fe59cb6
PainTrainee <vtec8197@gmail.com>
2023-12-14 15:41:51 +0700

    One To One

---
+++ P04_RelationDB/Models/Component.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P04_RelationDB.Models
+{
+    public class Component
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FeatureId { get; set; }
+        public Feature Feature { get; set; }
+    }
+}
