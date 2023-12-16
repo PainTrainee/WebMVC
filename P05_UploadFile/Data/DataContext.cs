a1681fd2c0bc51fb2e66cb2cf82d5895eefa56f0
PainTrainee <vtec8197@gmail.com>
2023-12-16 11:58:53 +0700

    UploadFile : Create Database

--- P05_UploadFile/Data/DataContext.cs
+++ P05_UploadFile/Data/DataContext.cs
@@ -4,6 +4,14 @@ namespace P05_UploadFile.Data
 {
     public class DataContext : DbContext
     {
-
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=LAPTOP-M8KR7TQE;Database=TestUpFile66;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- P05_UploadFile/Program.cs
+++ P05_UploadFile/Program.cs
@@ -1,7 +1,11 @@
+global using P05_UploadFile.Models;
+using P05_UploadFile.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
