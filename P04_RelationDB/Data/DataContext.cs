221332de2f7d387104d11c4e022a283c6e28102a
PainTrainee <vtec8197@gmail.com>
2023-12-16 10:25:57 +0700

    Extend Property From other Class

--- P04_RelationDB/Data/DataContext.cs
+++ P04_RelationDB/Data/DataContext.cs
@@ -13,9 +13,17 @@ namespace P04_RelationDB.Data
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer("Server=LAPTOP-M8KR7TQE;Database=TestProduct66;Trusted_Connection=True;TrustServerCertificate=True;");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //กำหนด Primary Key
+            modelBuilder.Entity<ComponentProduct>().HasKey(k => new { k.ProductId, k.ComponentId });
+        }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Component> Components { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<ComponentProduct> ComponentProducts { get; set; }
+        public DbSet<ProductDetail> ProductDetail { get; set; }
     }
 }
